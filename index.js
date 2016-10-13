97f5df16a7c3b453dc48bf48b8b3956daff5af3a
Tom Boutell <tom@punkave.com>
2016-10-13 07:16:30 -0400

    trace widget load calls and performance

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   construct: function(self, options) {
     self.apos.define('apostrophe-cursor', require('./lib/cursor.js'));
+    require('./lib/widgets.js')(self, options);
   }
 };
-
